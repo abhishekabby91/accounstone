fb26cee6730f7f0c5d61dba4e4ad598feea569ab
v0agent <it+v0agent@vercel.com>
2026-06-07 06:51:40 +0000

    feat: add initial CPA firm growth page component\n\nIntroduced a new page for CPA firm growth development.

---
+++ app/expertise/firm-growth/page.tsx
@@ -0,0 +1,8 @@
+export default function FirmGrowthPage() {
+  return (
+    <main className="min-h-screen p-10">
+      <h1>CPA Firm Growth</h1>
+      <p>Page under development.</p>
+    </main>
+  )
+}
